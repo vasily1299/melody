ba6833eac65814655bcd2912e94a8062d5b00e2e
vasily1299 <59225870+vasily1299@users.noreply.github.com>
2021-12-11 21:27:22 +0300

    бургер арбайтен

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.navbar-burger').click(function() {
+        $('.navbar-burger').toggleClass('open-burger');
+        $('.navbar-nav').toggleClass('open-burger');
+    });
+});
